97cf19e97b01db5dad2dfd4544b80bcf98c32be0
Alexandru Sima <alexsima203@gmail.com>
2025-04-14 21:55:32 +0300

    wip

--- web/src/components/nav-bar.tsx
+++ web/src/components/nav-bar.tsx
@@ -1,7 +1,16 @@
-'use client';
-import Link from 'next/link';
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    axios
+      .get("/api/auth/me")
+      .then((response) => setIsLoggedIn(response.status === 200));
+  }, []);
+
   return (
     <nav className="bg-gray-900 border-b border-gray-700">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -20,24 +29,34 @@ export default function NavBar() {
                 d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-9-3V11m0-5.5v-1a10 10 0 0120 0v1"
               />
             </svg>
-            <span className="ml-2 text-xl font-bold text-white">
-              ro-fish
-            </span>
+            <span className="ml-2 text-xl font-bold text-white">ro-fish</span>
           </Link>
-          <div className="flex space-x-4">
-            <Link
-              href="/login"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          {isLoggedIn ? (
+            <button
+              onClick={() => {
+                axios
+                  .get("/api/auth/logout", {})
+                  .then((_) => setIsLoggedIn(false));
+              }}
             >
-              Autentificare
-            </Link>
-            <Link
-              href="/register"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Înregistrare
-            </Link>
-          </div>
+              Delogare
+            </button>
+          ) : (
+            <div className="flex space-x-4">
+              <Link
+                href="/login"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Autentificare
+              </Link>
+              <Link
+                href="/register"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Înregistrare
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
