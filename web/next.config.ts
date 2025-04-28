fdb8623ed4aabe794e0bc4f420fd5adffd9e4b38
Alexandru Sima <alexsima203@gmail.com>
2025-04-28 22:31:42 +0300

    Add format- and lint-check for frontend (pre-commit hook)

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `http://${process.env.ROFISH_SERVER_ADDRESS || "localhost"}:${process.env.ROFISH_SERVER_PORT||"8080"}/api/:path*`,
+        destination: `http://${process.env.ROFISH_SERVER_ADDRESS || "localhost"}:${process.env.ROFISH_SERVER_PORT || "8080"}/api/:path*`,
       },
     ];
   },
